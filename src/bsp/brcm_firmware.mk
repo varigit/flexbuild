1df4b76ffaf85c1a985e24f9cd9b01eb9f42795c
FrancescoFerraro <francesco.f@variscite.com>
2026-01-16 16:20:44 +0100

    brcm_firmware: bc43xx: add symbolic link to firmware for each model
    Add symbolic link to firmware for DART-6UL and VAR-SOM-6UL SOMs.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- src/bsp/brcm_firmware.mk
+++ src/bsp/brcm_firmware.mk
@@ -15,7 +15,9 @@ brcm_firmware:
 	cd $(BSPDIR)/brcm_firmware/lib/firmware/brcm && \
 	for machine_sel in $(MACHINE_LIST); do \
 		ln -sf brcmfmac4339-sdio.bin brcmfmac4339-sdio.variscite,$$machine_sel.bin && \
-		ln -sf brcmfmac4339-sdio.txt brcmfmac4339-sdio.variscite,$$machine_sel.txt; \
+		ln -sf brcmfmac4339-sdio.txt brcmfmac4339-sdio.variscite,$$machine_sel.txt && \
+		ln -sf brcmfmac43430-sdio.bin brcmfmac43430-sdio.variscite,$$machine_sel.bin && \
+		ln -sf brcmfmac43430-sdio.txt brcmfmac43430-sdio.variscite,$$machine_sel.txt; \
 	done && \
 	mkdir -p $(FBOUTDIR)/bsp/brcm_firmware && \
 	cp -rf $(BSPDIR)/brcm_firmware/lib $(FBOUTDIR)/bsp/brcm_firmware && \
