19bac1c4c4a43d87cd2de49a7b404c55a0094802
Natalia Kovalenko <natasha.k@variscite.com>
2026-03-09 17:26:49 +0100

    utils: u_boot_fw_utils.mk: Fix path to the fw_env config file
    Since commit varigit/meta-variscite-bsp-imx@615134e73a2338f7907cf20841f1be0d1bab2c5e
    fw_env config file is located in the u-boot-variscite directory
    
    Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>
    (cherry picked from commit https://github.com/varigit-dev/flexbuild/commit/376c3d8ba48c)

--- src/apps/utils/u_boot_fw_utils.mk
+++ src/apps/utils/u_boot_fw_utils.mk
@@ -6,7 +6,7 @@ u_boot_fw_utils:
 	@[ $(SOCFAMILY) != IMX -o $(DISTROVARIANT) = base -o $(DISTROVARIANT) = tiny ] && exit || \
 	$(call fbprint_b,"u_boot_fw_utils") && \
 	$(call repo-mngr,fetch,meta_variscite_bsp_imx) && \
-	FWUTILS_DIR="$(PKGDIR)/meta_variscite_bsp_imx/recipes-bsp/u-boot/u-boot-fw-utils" && \
+	FWUTILS_DIR="$(PKGDIR)/meta_variscite_bsp_imx/recipes-bsp/u-boot/u-boot-variscite" && \
 	install -d $(DESTDIR)/etc && \
 	install -m 0644 $$FWUTILS_DIR/$(MACHINE)/fw_env.config $(DESTDIR)/etc/ && \
 	$(call fbprint_d,"u_boot_fw_utils")
